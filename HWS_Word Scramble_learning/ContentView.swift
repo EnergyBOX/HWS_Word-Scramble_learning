d7037a3c2d43a6ff10654dca341414410caad601
User <user@mail.net>
2024-01-28 19:56:18 +0200

    Initial Commit

---
+++ HWS_Word Scramble_learning/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HWS_Word Scramble_learning
+//
+//  Created by Server Admin on 28.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HWS_Word Scramble_learning/HWS_Word_Scramble_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_Word_Scramble_learningApp.swift
+//  HWS_Word Scramble_learning
+//
+//  Created by Server Admin on 28.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct HWS_Word_Scramble_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
